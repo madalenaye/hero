24e84db90fc2e00210c2617e50eb3298db139e94
Madalena Ye <madalena@pc-13-137.customer.ask4.lan>
2022-10-04 23:23:33 +0100

    improving

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Game {
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    private Screen screen;
+    public Game(){
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
