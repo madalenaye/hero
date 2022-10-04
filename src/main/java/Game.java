0b9e24f71d27c87df30ef35f48632aabc3559dfd
Madalena Ye <madalena@pc-13-137.customer.ask4.lan>
2022-10-04 23:15:00 +0100

    Oops exercise 6 again

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,21 +1,2 @@
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.swing.TerminalScrollController;
-
-import java.io.IOException;
-
-public class Game {
-    try {
-    Terminal terminal = new DefaultTerminalFactory().createTerminal();
-    Screen screen = new TerminalScreen(terminal);
-    screen.setCursorPosition(null);
-    screen.startScreen();
-    screen.doResizeIfNecessary();
-    } catch (IOException e) {
-    e.printStackTrace();
-    }
+package PACKAGE_NAME;public class Game {
 }
