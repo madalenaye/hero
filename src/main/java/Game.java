fd60ba03e8fe7280b004afb3fd42ae141f9d700b
Madalena Ye <madalena@pc-13-137.customer.ask4.lan>
2022-10-05 00:12:18 +0100

    yeey

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,6 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -7,15 +10,48 @@ import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
     public Game(){
         try {
             Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
+            this.screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null);
             screen.startScreen();
             screen.doResizeIfNecessary();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    private void draw() throws IOException{
+        screen.clear();
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+    public void run() throws IOException{
+        while(true){
+        draw();
+        KeyStroke key = screen.readInput();
+        processKey(key);
+        if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){screen.close();}
+        if (key.getKeyType() == KeyType.EOF) {
+            break;}
+        }
+    }
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()){
+            case ArrowRight:
+                this.x += 1;
+                break;
+            case ArrowLeft:
+                this.x -= 1;
+                break;
+            case ArrowUp:
+                this.y -= 1;
+                break;
+            case ArrowDown:
+                this.y += 1;
+                break;
         }
     }
 }
