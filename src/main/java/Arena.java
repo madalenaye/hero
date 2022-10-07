fc84fe319b2a2e283541da0abbb044d75c2d7b78
Madalena Ye <madalena@pc-13-137.customer.ask4.lan>
2022-10-07 18:39:03 +0100

    almost

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -3,15 +3,31 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
     private int width;
     private int height;
     private Hero hero;
+    private List<Wall> walls;
     public Arena(int width, int height) {
+        hero = new Hero(10,10);
         this.width = width;
         this.height = height;
-        hero = new Hero(10,10);
+        this.walls = createWalls();
+    }
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+        return walls;
     }
     public void processKey(KeyStroke key) throws IOException{
         switch (key.getKeyType()){
@@ -29,17 +45,22 @@ public class Arena {
                 break;
         }
     }
+
+    public void draw(TextGraphics graphics){
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#d9a8ff"));
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+        for (Wall wall : walls)
+            wall.draw(graphics);
+        this.hero.draw(graphics);
+    }
     public boolean canHeroMove(Position position){
-        if (position.getX() < width && position.getY() < height && position.getX() >= 0 && position.getY() >= 0) {return true;}
-        return false;
+        for (Wall wall : walls){
+            if (wall.getPosition().equals(position)){return false;}
+        }
+        return true;
     }
     private void moveHero(Position position) {
         if (canHeroMove(position)) {hero.setPosition(position);}
     }
 
-    public void draw(TextGraphics graphics){
-        graphics.setBackgroundColor(TextColor.Factory.fromString("#f56969"));
-        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
-        this.hero.draw(graphics);
-    }
 }
